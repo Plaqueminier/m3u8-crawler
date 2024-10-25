10e91c184d098c12a5195383d15e5770589cf172
Antoine <antoinebazin34@gmail.com>
2024-10-25 09:01:39 +0200

    fix

---
+++ scripts/fix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+find . -maxdepth 1 -type d -not -name "*$(date '+%Y-%m-%d')*" -not -name "." -not -name "node_modules" -not -name "src" -not -name "scripts" -not -name "videos" -not -name ".git" | while read folder; do
+    # Extract the base name of the folder (removing the path)
+    base_folder=$(basename "$folder")
+    
+    echo "Processing folder: $base_folder"
+    
+    # Run your command sequence, replacing 'key' with the folder name
+    pnpm ts-node src/fix.ts "$base_folder" "$base_folder" && \
+    rm -rf "$base_folder"/ && \
+    mv "$base_folder"*.ts videos
+done
